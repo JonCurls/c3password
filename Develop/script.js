93ae0a223c374e5e99d1c82c27aa3ab9422fe7c2
MrCurls <JonCurls@gmail.com>
2022-02-20 00:43:45 -0600

    design complete sans clean

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
 
   const upperCaseSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const lowerCaseSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -38,36 +33,31 @@ function writePassword() {
   var upperChoice = function () {
     var confirmUpper = window.confirm("Would you like UPPERCASE letters?");
     if (confirmUpper == true) {
-      passwordGen.push("a");
+      passwordGen.push("cap");
     };
     return confirmUpper;
   };
   var lowerChoice = function () {
     var confirmlower = window.confirm("Would you like lowercase letters?");
     if (confirmlower == true) {
-      passwordGen.push("b");
+      passwordGen.push("low");
     };
     return confirmlower;
   };
   var numericChoice = function () {
     var confirmNumeric = window.confirm("Would you like numbers?");
     if (confirmNumeric == true) {
-      passwordGen.push("c");
+      passwordGen.push("num");
     };
     return confirmNumeric;
   };
   var specCharChoice = function () {
     var confirmSpecChar = window.confirm("Would you like special characters?");
     if (confirmSpecChar == true) {
-      passwordGen.push("d");
+      passwordGen.push("spec");
     };
     return confirmSpecChar;
   };
-
-  // var randomUpper = Math.floor(Math.random() * upperCaseSet.length);
-  // var randomLower = Math.floor(Math.random() * lowerCaseSet.length);
-  // var randomNumeric = Math.floor(Math.random() * numericSet.length);
-  // var randomSpecChar = Math.floor(Math.random() * specCharSet.length);
 
   var passwordBits = {
     howMany: getLength(),
@@ -77,50 +67,98 @@ function writePassword() {
     specChar: specCharChoice(),
   };
 
-  if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.num == false && passwordBits.spec == false) {
+  if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.numeric == false && passwordBits.specChar == false) {
     window.alert("Please select at least 1 option.");
     writePassword();
   };
 
-//debugger;
+
+  // for (var i = 0; i < passwordBits.howMany; i++) {
+  //   switch (passwordGen[randomNumber(0, passwordGen.length)]) {
+  //     case 'cap':
+  //       password.push(upperCaseSet[randomNumber(1, 26)]);
+  //       //console.log(password);
+  //       break;
+  //     case 'low':
+  //       password.push(lowerCaseSet[randomNumber(1, 26)]);
+  //       break;
+  //     case 'num':
+  //       password.push(numericSet[randomNumber(1, 9)]);
+  //       break;
+  //     case 'spec':
+  //       password.push(specCharSet[randomNumber(1, 14)]);
+  //       break;
+  //   };
+  // };
+
   for (var i = 0; i < passwordBits.howMany; i++) {
-    switch (passwordGen[randomNumber(1, passwordGen.length)]) {
-      case "a":
-        password.push(upperCaseSet[randomNumber(1, 26)]);
-        //console.log(password);
-        break;
-      case "b":
-        password.push(lowerCaseSet[randomNumber(1, 26)]);
-        break;
-      case "c":
-        password.push(numericSet[randomNumber(0, 9)]);
-        break;
-      case "d":
-        password.push(specCharSet[randomNumber(0, 14)]);
-        break;
-    };
-  };
-
-
+    if (passwordBits.upperCase == true && passwordBits.lowerCase == true && passwordBits.numeric == true && passwordBits.specChar == true) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == true && passwordBits.numeric == true && passwordBits.specChar == false) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == true && passwordBits.numeric == false && passwordBits.specChar == true) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == false && passwordBits.numeric == true && passwordBits.specChar == true) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == true && passwordBits.numeric == false && passwordBits.specChar == false) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == false && passwordBits.numeric == true && passwordBits.specChar == false) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == false && passwordBits.numeric == false && passwordBits.specChar == true) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == true && passwordBits.lowerCase == false && passwordBits.numeric == false && passwordBits.specChar == false) {
+      password.push(upperCaseSet[randomNumber(1, 25)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == true && passwordBits.numeric == true && passwordBits.specChar == true) {
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == true && passwordBits.numeric == false && passwordBits.specChar == true) {
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == true && passwordBits.numeric == true && passwordBits.specChar == false) {
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+      password.push(numericSet[randomNumber(1, 9)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == true && passwordBits.numeric == false && passwordBits.specChar == false) {
+      password.push(lowerCaseSet[randomNumber(1, 25)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.numeric == true && passwordBits.specChar == true) {
+      password.push(numericSet[randomNumber(1, 9)]);
+      password.push(specCharSet[randomNumber(1, 13)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.numeric == true && passwordBits.specChar == false) {
+      password.push(numericSet[randomNumber(1, 9)]);
+    } else if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.numeric == false && passwordBits.specChar == true) {
+      password.push(specCharSet[randomNumber(1, 13)]);
+    }
+  }
 
   var finalPassword = password.join("");
 
+  //var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  //   console.log(passwordBits.howMany);
-  //   console.log(passwordBits.upperCase);
-  //   console.log(passwordBits.lowerCase);
-  //   console.log(passwordBits.numeric);
-  //   console.log(passwordBits.specChar);
+  passwordText.value = finalPassword.slice(0, passwordBits.howMany);
 
-  console.log(passwordGen.length);
-  console.log(passwordGen);
-  console.log(password);
-  console.log(finalPassword);
 
-  //   console.log(upperCaseSet[randomUpper]);
-  //   console.log(lowerCaseSet[randomLower]);
-  //   console.log(numericSet[randomNumeric]);
-  //   console.log(specCharSet[randomSpecChar]);
+  // console.log(passwordBits.howMany);
+  // console.log(passwordBits.upperCase);
+  // console.log(passwordBits.lowerCase);
+  // console.log(passwordBits.numeric);
+  // console.log(passwordBits.specChar);
+  // console.log(passwordGen.length);
+  // console.log(passwordGen);
+  // console.log(password.slice(0, passwordBits.howMany));
+  // console.log(finalPassword.slice(0, passwordBits.howMany));
 };
 
 
