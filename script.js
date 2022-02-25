bf1da45a734e33c26525c35bfea79d3b178e7a10
MrCurls <JonCurls@gmail.com>
2022-02-24 20:40:08 -0600

    final review and readME complete

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
+// Writes password
 function writePassword() {
-
+  //Password character sets
   const upperCaseSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const lowerCaseSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   const numericSet = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
@@ -18,18 +16,15 @@ function writePassword() {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
   }
-
-
+  //Determines the length of the password and validated password length
   var getLength = function () {
     var length = "";
     while (length === "" || length < 8 || length > 128) {
       length = prompt("Enter length of password (between 8 - 128 characters)");
     };
-    console.log("Your password length is " + length);
     return length;
   };
-
-
+  //Prompts for uppercase response
   var upperChoice = function () {
     var confirmUpper = window.confirm("Would you like UPPERCASE letters?");
     if (confirmUpper == true) {
@@ -37,6 +32,7 @@ function writePassword() {
     };
     return confirmUpper;
   };
+  //Prompts for lowercase response
   var lowerChoice = function () {
     var confirmlower = window.confirm("Would you like lowercase letters?");
     if (confirmlower == true) {
@@ -44,6 +40,7 @@ function writePassword() {
     };
     return confirmlower;
   };
+  //Prompts for numbers response
   var numericChoice = function () {
     var confirmNumeric = window.confirm("Would you like numbers?");
     if (confirmNumeric == true) {
@@ -51,6 +48,7 @@ function writePassword() {
     };
     return confirmNumeric;
   };
+  //Prompts for special chars response
   var specCharChoice = function () {
     var confirmSpecChar = window.confirm("Would you like special characters?");
     if (confirmSpecChar == true) {
@@ -58,7 +56,7 @@ function writePassword() {
     };
     return confirmSpecChar;
   };
-
+  //Stores users selection
   var passwordBits = {
     howMany: getLength(),
     upperCase: upperChoice(),
@@ -66,31 +64,12 @@ function writePassword() {
     numeric: numericChoice(),
     specChar: specCharChoice(),
   };
-
+  //Validates user has selected at least 1 option
   if (passwordBits.upperCase == false && passwordBits.lowerCase == false && passwordBits.numeric == false && passwordBits.specChar == false) {
     window.alert("Please select at least 1 option.");
     writePassword();
-  };
-
-
-  // for (var i = 0; i < passwordBits.howMany; i++) {
-  //   switch (passwordGen[randomNumber(0, passwordGen.length)]) {
-  //     case 'cap':
-  //       password.push(upperCaseSet[randomNumber(1, 26)]);
-  //       //console.log(password);
-  //       break;
-  //     case 'low':
-  //       password.push(lowerCaseSet[randomNumber(1, 26)]);
-  //       break;
-  //     case 'num':
-  //       password.push(numericSet[randomNumber(1, 9)]);
-  //       break;
-  //     case 'spec':
-  //       password.push(specCharSet[randomNumber(1, 14)]);
-  //       break;
-  //   };
-  // };
-
+  }
+  //Generates password based on users selection
   for (var i = 0; i < passwordBits.howMany; i++) {
     if (passwordBits.upperCase == true && passwordBits.lowerCase == true && passwordBits.numeric == true && passwordBits.specChar == true) {
       password.push(upperCaseSet[randomNumber(1, 25)]);
@@ -141,32 +120,10 @@ function writePassword() {
       password.push(specCharSet[randomNumber(1, 13)]);
     }
   }
-
+  //Compiles final password and prints
   var finalPassword = password.join("");
-
-  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = finalPassword.slice(0, passwordBits.howMany);
-
-
-  // console.log(passwordBits.howMany);
-  // console.log(passwordBits.upperCase);
-  // console.log(passwordBits.lowerCase);
-  // console.log(passwordBits.numeric);
-  // console.log(passwordBits.specChar);
-  // console.log(passwordGen.length);
-  // console.log(passwordGen);
-  // console.log(password.slice(0, passwordBits.howMany));
-  // console.log(finalPassword.slice(0, passwordBits.howMany));
 };
-
-
-// Add event listener to generate button
+//Event listener for generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
